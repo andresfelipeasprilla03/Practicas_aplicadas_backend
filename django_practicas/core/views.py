1020f5324976431c09481107e10b6a65a8713b29
andresfelipeasprilla03 <andresfelipeasprilla03@gmail.com>
2025-11-16 18:45:33 -0500

    creacion de urls y templates

--- django_practicas/core/views.py
+++ django_practicas/core/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
 # Create your views here.
+#Muestra la template home.html
+
+def home(request):
+ return render(request, "home.html", {"title": "Hola Django!"})
